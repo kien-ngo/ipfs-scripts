0d5e21ba93105876bd88c118fe0f17f2be02545c
Kien Ngo <kenseverus@gmail.com>
2023-03-23 21:32:17 -0400

    Create pin-json.sh

---
+++ pin-json.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set the name of the JSON file containing the list of CIDs and folder names
+filename="cids.json"
+
+# Read the JSON file line by line, assuming it contains an array of objects
+jq -c '.[]' "$filename" | while read -r line; do
+    # Extract the CID and folder name from the line using jq
+    cid="$(echo "$line" | jq -r '.cid')"
+    foldername="$(echo "$line" | jq -r '.folderName')"
+
+    # Execute the command for each line
+    ipfs files cp /ipfs/"$(ipfs pin add "$cid" | cut -f 2 -d ' ')" "/$foldername"
+done
